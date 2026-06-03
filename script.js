e709ce17d0e4a11c9f78d151b494be9aa8e1e3fc
rogerio <oliveirarogerio585@gmail.com>
2026-06-03 18:28:34 -0300

    Adicionando os últimos links. edição do header para center

--- script.js
+++ script.js
@@ -5,44 +5,119 @@ const sectors = [
     mapsLink: "https://maps.app.goo.gl/MRhLXy6bJ3bVGxHe9?g_st=iw"
   },
   {
-    id: 2, name: "Lab. de Informática",
+    id: 2, name: "UINFOR",
     emoji: "💻", bg: "#e8eff8", color: "#1a4f8a",
-    mapsLink: "https://maps.app.goo.gl/informatica"
+    mapsLink: "https://maps.app.goo.gl/PgY9vYw9RyCKbzCX9?g_st=iw"
   },
   {
-    id: 3, name: "Salas de Aula",
+    id: 3, name: "CETEP",
     emoji: "🏫", bg: "#e8eff8", color: "#1a4f8a",
-    mapsLink: "https://maps.app.goo.gl/salas",
+    mapsLink: "https://maps.app.goo.gl/jKeSiWnyuymdCfnW6?g_st=iw",
   },
   {
-    id: 4, name: "Secretaria Acadêmica",
-    emoji: "📋", bg: "#e6f2ea", color: "#2e6b3e",
-    mapsLink: "https://maps.app.goo.gl/secretaria"
+    id: 4, name: "Lab. de Solos",
+    emoji: "🌍", bg: "#e6f2ea", color: "#2e6b3e",
+    mapsLink: "https://maps.app.goo.gl/VaVuPpTc8Xxs8naA6?g_st=iw"
   },
   {
-    id: 5, name: "Recursos Humanos",
+    id: 5, name: "Antiga Reitoria",
     emoji: "👥", bg: "#e6f2ea", color: "#2e6b3e",
-    mapsLink: "https://maps.app.goo.gl/rh"
+    mapsLink: "https://maps.app.goo.gl/hw5oDcpRH94o4UXDA?g_st=iw"
   },
   {
-    id: 6, name: "Setor Financeiro",
-    emoji: "💰", bg: "#e6f2ea", color: "#2e6b3e",
-    mapsLink: "https://maps.app.goo.gl/financeiro"
+    id: 6, name: "Almoxarifado",
+    emoji: "📦", bg: "#e6f2ea", color: "#2e6b3e",
+    mapsLink: "https://maps.app.goo.gl/gXofGqXHkMusNLUz9?g_st=iw"
   },
   {
-    id: 7, name: "Restaurante Universitário",
-    emoji: "🍽️", bg: "#fdf0d8", color: "#8a5a0a",
-    mapsLink: "https://maps.app.goo.gl/restaurante"
+    id: 7, name: "Prefeitura de campus",
+    emoji: "🏛️", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/FMpJU6eq5pS3HuPa9?g_st=iw"
   },
   {
-    id: 8, name: "Estacionamento",
-    emoji: "🅿️", bg: "#fdf0d8", color: "#8a5a0a",
-    mapsLink: "https://maps.app.goo.gl/estacionamento"
+    id: 8, name: "Luisão",
+    emoji: "🏫", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/EruhgNuXwCcxva5B8?g_st=iw"
   },
   {
-    id: 9, name: "Quadras Esportivas",
+    id: 9, name: "Módulo Amélia Barreto",
+    emoji: "🔬", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/GReT2ya1QDGMxXWR6?g_st=iw"
+  },
+  {
+    id: 10, name: "CEUAS",
+    emoji: "🏥", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/aiAWWk6RibUhrTvL8?g_st=iw"
+  },
+  {
+    id: 11, name: "Academia",
+    emoji: "🏋️", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/DGVCkQ1b1LM6tsgu7?g_st=iw"
+  },
+  {
+    id: 12, name: "Acadêmico",
+    emoji: "📚", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/vbaetrWurEFfe9NQ7?g_st=iw"
+  },
+  {
+    id: 13, name: "Quiosque",
+    emoji: "🍟", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/fFWUZjjw1HnHNWVf8?g_st=iw"
+  },
+  {
+    id: 14, name: "Módulo I",
+    emoji: "🏫", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/1FK7AFrsNvQh5h619?g_st=iw"
+  },
+  {
+    id: 15, name: "Módulo II",
+    emoji: "🏫", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/UDrSXkRWQxmke4gQ8?g_st=iw"
+  },
+  {
+    id: 16, name: "Quadra",
     emoji: "🏀", bg: "#fdf0d8", color: "#8a5a0a",
-    mapsLink: "https://maps.app.goo.gl/quadras"
+    mapsLink: "https://maps.app.goo.gl/Ejg276rnnoj3PR8g8?g_st=iw"
+  },
+  {
+    id: 17, name: "Estação Metereológica",
+    emoji: "🌥️", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/vhyRKQSSv2N93LWk9?g_st=iw"
+  },
+    {
+    id: 18, name: "Viveiro",
+    emoji: "🍀", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/Qejp6tZrkH6TMcX46?g_st=iw"
+  },
+  {
+    id: 19, name: "Módulo de Psicologia",
+    emoji: "🧑‍⚕️", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/8su88YHPi9H1SRj3A?g_st=iw"
+  },
+  {
+    id: 20, name: "Módulo de Engenharia Florestal",
+    emoji: "🌲", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/jKaP2g24kN8ecVTg8?g_st=iw"
+  },
+  {
+    id: 21, name: "RU - Restaurante Universitário",
+    emoji: "🍲", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/WQPmfRPC49fua7ma9?g_st=iw"
+  },
+  {
+    id: 22, name: "Módulo de Medicina",
+    emoji: "🩺", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/NaLr88K5RC8evJ3WA?g_st=iw"
+  },
+  {
+    id: 23, name: "Reitoria",
+    emoji: "👨‍💼", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/ootw5r6X1HevbqG77?g_st=iw"
+  },
+  {
+    id: 24, name: "Lab. de Água",
+    emoji: "💧", bg: "#fdf0d8", color: "#8a5a0a",
+    mapsLink: "https://maps.app.goo.gl/ESbuSjFvx7roMtYk9?g_st=iw"
   }
 ];
 
